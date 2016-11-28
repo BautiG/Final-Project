f4649418b68c8d540def6600b38139663779aed2
BautiG <juanbautistagallino@hanovernorwichschools.org>
2016-11-28 13:59:49 -0500

    Updated from Brython Server: 11/28/2016 1:59:49 PM

--- Final.py
+++ Final.py
@@ -1,1 +1,9 @@
+"""
+Author: <your name here>
+Credit: <list sources used, if any>
+"""
+from ggame import App, Sprite, ImageAsset, Frame
+from ggame import SoundAsset, Sound, TextAsset, Color
+import math
 
+class player1(sprite)
