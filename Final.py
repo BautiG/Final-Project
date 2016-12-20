02c6f9c35e420e9d9f74cb18958d0f636011a332
BautiG <juanbautistagallino@hanovernorwichschools.org>
2016-12-20 13:37:20 -0500

    Updated from Brython Server: 12/20/2016 1:37:19 PM

--- Final.py
+++ Final.py
@@ -133,7 +133,7 @@ app = BrickBreaker(0, 0)
 app.run()
 """
 class Background(Sprite):
-    asset = RectangleAsset(1727, 800, brickborder, white)
+    asset = RectangleAsset(1727, 859, brickborder, white)
     height= 100
     width= 100
     
